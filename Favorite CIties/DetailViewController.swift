ec3e2e67354925411303a5c1aa77b67832099f74
Matthew Harkey <matthewharkey@matthews-macbook-air-65.local>
2019-02-01 11:41:41 -0600

    Connected UI objects to storyboard

--- Favorite CIties/DetailViewController.swift
+++ Favorite CIties/DetailViewController.swift
@@ -9,32 +9,45 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
-            }
-        }
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        configureView()
-    }
-
-    var detailItem: NSDate? {
+    
+    @IBOutlet weak var cityTesxtField: UITextField!
+    @IBOutlet weak var statetextField: UITextField!
+    @IBOutlet weak var populationTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var detailItem: City? {
         didSet {
             // Update the view.
             configureView()
         }
     }
 
+    
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+    
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let city = self.detailItem {
+            if cityTesxtField != nil {
+                cityTesxtField.text = city.name
+                statetextField.text = city.state
+                populationTextField.text = String(city.population)
+                imageView.image = UIImage(data: city.image)
+            }
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        if let city = self.detailItem {
+            city.name = cityTesxtField.text!
+            city.state = statetextField.text!
+            city.population = Int(populationTextField.text!)!
+        }
+        }
 }
 
